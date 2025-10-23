bd4baeb9f01e83d2e5b0a7a3ae459635cd86fd2b
Jean Claude PASTOR <jcpastor.pro@gmail.com>
2025-10-24 00:28:30 +0200

    correction rfidchip

--- Client/Models/RfidChipResponse.cs
+++ Client/Models/RfidChipResponse.cs
@@ -10,4 +10,6 @@ public class RfidChipResponse
     public string Status { get; set; } = string.Empty;
     public string? Description { get; set; }
     public DateTime ActivationDate { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public string? PackagingCode { get; set; }
 }
