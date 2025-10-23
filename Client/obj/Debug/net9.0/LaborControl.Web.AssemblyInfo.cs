f3b07a8d144b66927a9c3448766634075e2b2b5d
Jean Claude PASTOR <jcpastor.pro@gmail.com>
2025-10-23 23:50:42 +0200

    MODULE GESTION PUCES RFID

--- Client/obj/Debug/net9.0/LaborControl.Web.AssemblyInfo.cs
+++ Client/obj/Debug/net9.0/LaborControl.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LaborControl.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da2a2d27efce2f622d4d267001f3b5f9a7c4accc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ad60a07f8fac57d3b16506db01d5d7e5b849ed1")]
 [assembly: System.Reflection.AssemblyProductAttribute("LaborControl.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LaborControl.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
